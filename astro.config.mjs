911cc39ece03dc649ae81d9aa6cadb8b3d4fccda
Eric Bruet <nom@nom.digital>
2026-01-12 08:13:25 +0100

    chore: align astro config with nom

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import remarkBreaks from "remark-breaks";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: "https://oui.nom.digital",
+
+  integrations: [sitemap()],
+
+  markdown: {
+    remarkPlugins: [remarkBreaks],
+  },
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
